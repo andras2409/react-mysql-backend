6528556ddebd8e74691bd9aad275581ac3f528ac
andras2409 <andras.nagy2002@gmail.com>
2024-04-13 01:33:57 +0200

    dependencies changed

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const cors = require('cors');
 const db = require('./databaseConnection.js'); // The file you created for your database connection
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path');
 
 app.use(cors()); // This enables CORS for all routes
-app.use(express.static('public'));
+
+// Point to the build directory of your React app
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// All routes not handled by the static middleware should redirect to index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.get('/employees', (req, res) => {
     db.query('SELECT * FROM employees', (err, results) => {
