62195a8a498f593d517b5ce9dca4a5d84ccc29e2
andras2409 <andras.nagy2002@gmail.com>
2024-04-13 02:20:15 +0200

    update

--- server.js
+++ server.js
@@ -6,25 +6,6 @@ const port = process.env.PORT || 3000;
 
 app.use(cors()); // This enables CORS for all routes
 
-const path = require('path');
-
-// Serve static files from 'build' directory
-app.use(express.static(path.join(__dirname, 'build')));
-
-// Handle SPA routing by returning index.html for all routes
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.use((req, res, next) => {
-    if (req.path.endsWith('.js')) {
-        res.type('text/javascript');
-    } else if (req.path.endsWith('.css')) {
-        res.type('text/css');
-    }
-    next();
-});
-
 app.get('/employees', (req, res) => {
     db.query('SELECT * FROM employees', (err, results) => {
         if (err) throw err;
